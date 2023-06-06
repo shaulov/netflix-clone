e48e7ed3efce62978eff64bc9d127eef4e0a5c4d
shaulov <shineginn@gmail.com>
2023-06-06 17:42:04 +0300

    Add rout handler for api/current

---
+++ app/api/current/route.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from 'next/server';
+import serverAuth from '@/lib/server-auth';
+
+export async function GET(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    if (request.method !== 'GET') {
+      return new NextResponse('Method Not Allowed', { status: 405 });
+    }
+
+    const { currentUser } = await serverAuth(request, response);
+
+    return NextResponse.json(currentUser);
+  } catch (error) {
+    return new NextResponse(`Something went wrong: ${error}}`, { status: 400 });
+  }
+}
