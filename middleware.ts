abe2c7b3c103add0d5d52bd6c9022bd967545e4e
shaulov <shineginn@gmail.com>
2023-06-16 02:53:54 +0300

    Add middleware for routes protection

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/auth', request.url))
+}
+
+export const config = { 
+  matcher: [
+    "/",
+    "/profiles/:path*",
+  ]
+};
