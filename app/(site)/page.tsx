361418573e1521d382cc9dabd5c0e869b9722504
shaulov <shineginn@gmail.com>
2023-06-22 22:35:03 +0300

    Fix getting user data in serverAuth

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,16 +2,13 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { signOut, useSession } from 'next-auth/react';
-import useCurrentUser from '@/hooks/use-current-user';
+import { useSession } from 'next-auth/react';
 import { AppRoute, AuthStatus } from '@/const';
 import Navbar from '@/components/navbar';
 
 export default function Home() {
   const session = useSession();
   const router = useRouter();
-
-  // const { data: user } = useCurrentUser();
 
   useEffect(() => {
     if (session?.status === AuthStatus.NoAUTH) {
